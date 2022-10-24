f880f95b26d6d5e4e3e6a87a534449342d644a43
NastyaZiss <kurbatova.vladuslava050@gmail.com>
2022-10-24 09:28:09 +0300

    Add а double_text_widget , which also is in the home_screen and also with search_screen. Add a photo for search_screen.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:android_s_fl/screens/hotel_screen.dart';
 import 'package:android_s_fl/screens/ticket_view.dart';
 import 'package:android_s_fl/utils/app_info_list.dart';
+import 'package:android_s_fl/widgets/double_text_widget.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
@@ -74,18 +75,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
                 Gap(40),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Upcoming Flights", style: Styles.headLineStyle2,),
-                    InkWell(
-                      onTap: (){
-
-                      },
-                        child: Text("View all", style: Styles.textStyle.copyWith(color: Styles.primaryColor),),),
-
-                  ],
-                ),
+                const AppDoubleTextWidget(bigText: "Upcoming Flight", smallText: "View all"),
               ],
 
             ),
@@ -101,19 +91,10 @@ class _HomeScreenState extends State<HomeScreen> {
           const Gap(15),
           Container(
             padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Hotels",style: Styles.headLineStyle2,),
-                InkWell(
-                  onTap: (){
-
-                  },
-                  child: Text("View all", style: Styles.textStyle.copyWith(color: Styles.primaryColor),),),
-
-              ],
-            ),
+            child:
+            const AppDoubleTextWidget(bigText: "Hotels", smallText: "View all"),
           ),
+          const Gap(15),
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             padding: const EdgeInsets.only(left: 20),
